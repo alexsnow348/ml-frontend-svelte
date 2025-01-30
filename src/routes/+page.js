84c29a8eb2af7f2ed81f045508105918c7afe17c
Alex Snow <alexsnow348@gmail.com>
2025-01-30 17:12:28 +0100

    updated for getting general info

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,29 @@
-export async function load({ fetch }) {
-	const res = await fetch('/api/v1/dashboard/cellcounting/experiments');
-	const data = await res.json();
-	if (res.ok) {
-		return { data };
-	}
+export async function load({ fetch, params }) {
+	const experimentsRes = await fetch('/api/v1/dashboard/cellcounting/experiments');
+	const dashboardData = await experimentsRes.json();
+	// get length of the experiments
+	const uniqueExperimentUuid = [
+		...new Set(dashboardData.experiments_list.map((item) => item.transaction_id))
+	];
+	// loop through the experiments and get the data for each experiment
+	for (let i = 0; i < uniqueExperimentUuid.length; i++) {
+		const uniqueDetailRes = await fetch(
+			`/api/v1/dashboard/cellcounting/unique_details/${uniqueExperimentUuid[i]}`
+		);
+		const experimentData = await uniqueDetailRes.json();
+		// console.log(experimentData.unique_details);
+		// add the data to the experiments_list
+		dashboardData.experiments_list[i].uniqueDetail = experimentData.unique_details;
 
+		// // get the data for the experiment
+		// const summaryDataRes = await fetch(
+		// 	`/api/v1/dashboard/cellcounting/summary/${uniqueExperimentUuid[i]}`
+		// );
+		// const summaryData = await summaryDataRes.json();
+		// // add the data to the experiments_list
+		// dashboardData.experiments_list[i].summaryData = summaryData;
+	}
+	// console.log(dashboardData.experiments_list[0].uniqueDetail);
+	// console.log(dashboardData.experiments_list[0].summaryData);
+	return { dashboardData };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,38 @@
 
 	import { toggleMode } from 'mode-watcher';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	let { data } = $props();
+	const fruits = [
+		{ value: 'apple', label: 'Apple' },
+		{ value: 'banana', label: 'Banana' },
+		{ value: 'blueberry', label: 'Blueberry' },
+		{ value: 'grapes', label: 'Grapes' },
+		{ value: 'pineapple', label: 'Pineapple' }
+	];
+
+	let value = $state('');
+	let experiment_name = $state('');
+	const experiments = data.dashboardData.experiments_list;
+	// derived unique details from experiments
+	// const uniqueRunName = experiments.map((experiment) => ({
+	const uniqueRunName = [];
+	const uniqueRunVersion = [];
+	for (let i = 0; i < experiments.length; i++) {
+		const runName = experiments[i].uniqueDetail.run_name;
+		for (let i = 0; i < runName.length; i++) {
+			uniqueRunName.push({ value: runName[i], label: runName[i] });
+		}
+		const runVersion = experiments[i].uniqueDetail.run_version;
+		for (let i = 0; i < runVersion.length; i++) {
+			uniqueRunVersion.push({ value: runVersion[i], label: runVersion[i] });
+		}
+
+	}
+	const triggerExperiment = $derived(experiments.find((experiment) => experiment.experiment_name === experiment_name)?.experiment_name ?? 'Select an Experiment');
+	const triggerRunVersion = $derived(uniqueRunVersion.find((version) => version.value === value)?.label ?? 'Select a Run Version');
+	const triggerRunName = $derived(uniqueRunName.find((name) => name.value === value)?.label ?? 'Select a Run Name');
+	const triggerWell = $derived(fruits.find((f) => f.value === value)?.label ?? 'Select a Well');
 </script>
 
 <Sidebar.Provider>
@@ -32,23 +64,86 @@
 				</Breadcrumb.Root>
 			</div>
 		</header>
-		<Button onclick={toggleMode} variant="outline" size="icon" class="absolute top-2 right-2">
-	<Sun
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 "
-	/>
-	<Moon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
-	<span class="sr-only">Toggle theme</span>
-</Button>
+		<Button onclick={toggleMode} variant="outline" size="icon" class="absolute right-2 top-2">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 "
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	
+		<div class="grid auto-rows-min gap-4 p-5 pt-0 md:grid-cols-4">
+			<div>
+				<Select.Root type="single" name="selected Experiment" bind:value>
+					<Select.Trigger class="aspect-video rounded-xl bg-muted/50">
+						{triggerExperiment}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.GroupHeading>Experiment</Select.GroupHeading>
+							{#each experiments as experiment}
+								<Select.Item value={experiment.experiment_name} label={experiment.experiment_name}>{experiment.experiment_name}</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<div>
+				<Select.Root type="single" name="selectedRunVersion" bind:value>
+					<Select.Trigger class="aspect-video rounded-xl bg-muted/50">
+						{triggerRunVersion}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.GroupHeading>Run Version</Select.GroupHeading>
+							{#each uniqueRunVersion as version}
+								<Select.Item value={version.value} label={version.label}>{version.label}</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<div>
+				<Select.Root type="single" name="selectedRunName" bind:value>
+					<Select.Trigger class="aspect-video rounded-xl bg-muted/50">
+						{triggerRunName}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.GroupHeading>Run Name</Select.GroupHeading>
+							{#each uniqueRunName as name}
+								<Select.Item value={name.value} label={name.label}>{name.label}</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<div>
+				<Select.Root type="single" name="selectedWell" bind:value>
+					<Select.Trigger class="aspect-video rounded-xl bg-muted/50">
+						{triggerWell}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.GroupHeading>Well</Select.GroupHeading>
+							{#each fruits as fruit}
+								<Select.Item value={fruit.value} label={fruit.label}>{fruit.label}</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+			</div>
+		</div>
+		<!-- Summary -->
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
 			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-				<div class="aspect-video rounded-xl bg-muted/50">
-					<!-- Add Card here -->
-				</div>
+				<div class="aspect-video rounded-xl bg-muted/50"></div>
 				<div class="aspect-video rounded-xl bg-muted/50"></div>
 				<div class="aspect-video rounded-xl bg-muted/50"></div>
 			</div>
+			<!-- Graph -->
 			<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
 		</div>
 	</Sidebar.Inset>
