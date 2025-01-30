f20aebddf8061c6819918ca65463e26cbfe31091
Alex Snow <alexsnow348@gmail.com>
2025-01-30 11:49:01 +0100

    update for layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	let { children } = $props();
 </script>
 <ModeWatcher />
-<Sidebar.Provider>
-	<AppSidebar />
-	<main>
-		<Sidebar.Trigger />
-		{@render children?.()}
-	</main>
-</Sidebar.Provider>
+{@render children?.()}
+
