fab3bf44b02faf31274b5e3469610d85bd3d993f
Alex Snow <alexsnow348@gmail.com>
2025-01-30 10:24:11 +0100

    updated for sidebar

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,8 +2,7 @@ export async function load({ fetch }) {
 	const res = await fetch('/api/v1/dashboard/cellcounting/experiments');
 	const data = await res.json();
 	if (res.ok) {
-		console.log('data', data);
 		return { data };
 	}
-	console.log('data', data);
+
 }
